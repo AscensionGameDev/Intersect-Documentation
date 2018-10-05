45d3385ae0aa92345eda77be1566c67ebf48c87b
JC Snider <jcsnider@users.noreply.github.com>
2018-10-05 17:44:59 -0400

    Fixing base url again..

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: '/Intersect-Documentation-Pages/',
+    base: '/',
     title: 'Intersect',
     description: 'The free, open-source, modern 2d mmorpg maker.',
     ga: 'UA-87324178-1',
