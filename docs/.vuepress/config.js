8dab89003fc4db5573ca5fb6714a6deb5bca1084
JC Snider <jcsnider@users.noreply.github.com>
2019-03-16 16:36:39 -0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,36 @@ module.exports = {
     title: 'Intersect',
     description: 'The free, open-source, modern 2d mmorpg maker.',
     ga: 'UA-87324178-1',
+    themeConfig: {
+        repo: 'jcsnider/Intersect-Engine',
+        docsRepo: 'AscensionGameDev/Intersect-Documentation',
+        repoLabel: 'Contribute',
+        editLinkText: 'Help us improve this page.',
+        editLinks: true,
+        nav: [
+            {
+                text: 'Get Started',
+                link: '/',
+            },
+            {
+                text: 'Community Guides',
+                link: 'https://www.ascensiongamedev.com/forum/56-guides-and-tutorials/',
+            },
+            {
+                text: 'Get Help',
+                link: 'https://www.ascensiongamedev.com/forum/53-questions-and-answers/'
+            }
+        ],
+        sidebar: {
+            '/': [
+                {
+                    title: 'Welcome',
+                    collapsable: false,
+                    children: [
+                        '/project/introduction.md',
+                    ]
+                },
+            ],
+        },
+    },
 };
