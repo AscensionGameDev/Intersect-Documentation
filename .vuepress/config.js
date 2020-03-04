153341088af952e39a4197000bc50e276c946c3a
JC Snider <jcsnider3@gmail.com>
2020-03-04 12:11:52 -0500

    Basic visual studio install steps

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -77,7 +77,6 @@ module.exports = {
                     collapsable: true,
                     children: [
                         '/dev/',
-                        '/dev/introduction/repo.md',
                     ]
                 },
                 {
