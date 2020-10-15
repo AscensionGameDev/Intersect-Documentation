2e9a38c118ae73946b8f0976029b3b21442ef927
lodicolo <lodico.rj@gmail.com>
2020-10-15 15:04:27 -0400

    Update es.js

--- docs/.vuepress/locales/es.js
+++ docs/.vuepress/locales/es.js
@@ -80,7 +80,7 @@ module.exports = {
                     ]
                 },
                 {
-                    title: 'Repository',
+                    title: '<<todo:es>>Repository',
                     collapsable: true,
                     children: [
                         '/developer/repository/',
